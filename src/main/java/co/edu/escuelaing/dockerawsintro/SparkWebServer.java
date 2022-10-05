0adc9f5a51bd2768503213e3e48073f9c53ca416
unknown <laura.alvarado-g@labinfo.is.escuelaing.edu.co>
2022-10-05 16:22:54 -0500

    first commit

---
+++ src/main/java/co/edu/escuelaing/dockerawsintro/SparkWebServer.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package co.edu.escuelaing.dockerawsintro;
+
+import static spark.Spark.*;
+
+
+public class SparkWebServer {
+    
+    public static void main(String... args){
+          port(getPort());
+          get("hello", (req,res) -> "Hello Docker!");
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+    
+}
