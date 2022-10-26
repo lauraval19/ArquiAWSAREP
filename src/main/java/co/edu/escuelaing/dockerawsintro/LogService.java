71007c73be17fdc48a5abc87cd05cc62a0c8604b
unknown <laura.alvarado-g@labinfo.is.escuelaing.edu.co>
2022-10-26 15:56:47 -0500

    docker

--- src/main/java/co/edu/escuelaing/dockerawsintro/LogService.java
+++ src/main/java/co/edu/escuelaing/dockerawsintro/LogService.java
@@ -13,14 +13,14 @@ public class LogService {
     public static void main(String... args){
           port(getPort());
           staticFiles.location("/public");
-          HttpConnection httPconnection = new HttpConnection();
+          MongoConnection mongoConnection = new MongoConnection();
 
         get("/mongo", (req, res) -> {
             res.type("application/json");
             String cadena = req.queryParams("cad");
             System.out.println("LLEGA A /MONGO");
             System.out.println(cadena);
-            return httPconnection.mongodb(cadena);
+            return mongoConnection.mongodb(cadena);
         });
         }
 
--- src/main/java/co/edu/escuelaing/dockerawsintro/MongoConnection.java
+++ src/main/java/co/edu/escuelaing/dockerawsintro/MongoConnection.java
@@ -2,30 +2,22 @@ package co.edu.escuelaing.dockerawsintro;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.ServerApi;
-import com.mongodb.ServerApiVersion;
 import com.mongodb.client.*;
 import org.bson.Document;
-import org.bson.types.ObjectId;
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
-import java.net.URL;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class HttpConnection {
+public class MongoConnection {
 
     private static final String USER_AGENT = "Mozilla/5.0";
     private String url = "mongodb://localhost:27017";
 
-    public String mongodb(String user) throws IOException, IOException {
+    public List<Document> mongodb(String user) throws IOException, IOException {
         String connstr = url;
 
         ConnectionString connectionString = new ConnectionString(connstr);
@@ -48,21 +40,26 @@ public class HttpConnection {
         FindIterable<Document> iterable = cadenas.find();
         MongoCursor<Document> cursor = iterable.iterator();
 
+
+
+        Document cadenan = new Document();
+        Date date = Date.from(Instant.now());
+        cadenan.append("Cadena", user);
+        cadenan.append("Fecha",date);
+
+        cadenas.insertOne(cadenan);
+
+        List<Document> logs = new ArrayList<>();
         //Recorre el iterador obtenido del iterable
         while (cursor.hasNext()) {
             System.out.println("desdee el iterador------->");
             System.out.println(cursor.next());
+            if(logs.size() <= 10){
+                logs.add(cursor.next());
+            }
         }
 
-        Document cadenan = new Document();
-        Date date = Date.from(Instant.now());
-        cadenan.put("Cadena", user);
-        cadenan.put("Fecha",date);
-
-        cadenas.insertOne(cadenan);
-
-        List<Document> databasesn = mongoClient.listDatabases().into(new ArrayList<>());
-        return databasesn.get(0).toJson();
+        return logs;
     }
 
 }
